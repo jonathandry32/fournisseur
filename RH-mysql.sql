4e5e3312180f48a9e40282c0d72aec7b58c9a3e4
jonathandry32 <bayronkevin1@gmail.com>
2023-11-25 07:54:10 +0300

    fonction

--- RH-mysql.sql
+++ RH-mysql.sql
@@ -1,4 +1,3 @@
--- Création de la table Entreprises
 CREATE TABLE Entreprises (
     idEntreprise SERIAL PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
@@ -6,20 +5,29 @@ CREATE TABLE Entreprises (
     telephone VARCHAR(20),
     adresse TEXT
 );
+-- insertion
+--liste
 
--- Création de la table Produits
 CREATE TABLE Produits (
     idProduit SERIAL PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
     unite VARCHAR(50),
     prix NUMERIC(10,2)
 );
+-- insertion
+--liste
+--update
 
--- Création de la table Stocks
 CREATE TABLE Stocks (
     idStock SERIAL PRIMARY KEY,
     idProduit INT REFERENCES Produits(idProduit),
     quantite DOUBLE PRECISION,
     prixUnitaire DOUBLE PRECISION,
-    type VARCHAR(50)
+    type VARCHAR(50),
+    date DATE
 );
+--insertion
+-- liste (historique: select *)
+
+-- etat stock sum(type=entrer)-sum(type=sortie) where idProduit
+
